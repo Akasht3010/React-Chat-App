01fbeaaea1f060a0a1548f081f006bd7e5696deb
Akasht3010 <akashthakkar931@gmail.com>
2024-05-08 22:06:10 +0530

    till chat.css

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -4,7 +4,33 @@ import './chat.css'
 const Chat = () => {
   return (
     <div className='chat'>
-      Chat
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, iure!</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder='Type a message' />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" />
+        </div>
+        <button className='sendButton'>Send</button>
+      </div>
     </div>
   )
 }
